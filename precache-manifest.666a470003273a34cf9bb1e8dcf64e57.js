c9262b3c45ac83f9c13e05dfdbb1ff45154b32a1
Ivan <ivanpejanic@outlook.com>
2020-09-15 15:18:40 +0200

    Updates

---
+++ precache-manifest.666a470003273a34cf9bb1e8dcf64e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edd5b10e10e969d27dd7dc88f10e94e",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "30ad59dedcff9fc1796e",
+    "url": "/task-tracker/static/css/main.c53fdaea.chunk.css"
+  },
+  {
+    "revision": "fafebb12e21b36585ba9",
+    "url": "/task-tracker/static/js/2.78ca21d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-tracker/static/js/2.78ca21d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ad59dedcff9fc1796e",
+    "url": "/task-tracker/static/js/main.084c72db.chunk.js"
+  },
+  {
+    "revision": "44e2746e3f7572562cbe",
+    "url": "/task-tracker/static/js/runtime-main.290efcd8.js"
+  }
+]);
