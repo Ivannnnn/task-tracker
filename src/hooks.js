26edad70f27ee77ae6b8e61983b28b700eaaa67b
Ivan <ivanpejanic@outlook.com>
2020-09-15 15:40:18 +0200

    Update: router to hashLocation

--- src/hooks.js
+++ src/hooks.js
@@ -107,3 +107,22 @@ export const useOrderable = (orderBy) => {
 
   return [items, { set, add, remove, update, move }]
 }
+
+const currentLocation = () => {
+  return window.location.hash.replace(/^#/, '') || '/'
+}
+
+export const useHashLocation = () => {
+  const [loc, setLoc] = useState(currentLocation())
+
+  useEffect(() => {
+    const handler = () => setLoc(currentLocation())
+
+    window.addEventListener('hashchange', handler)
+    return () => window.removeEventListener('hashchange', handler)
+  }, [])
+
+  const navigate = useCallback((to) => (window.location.hash = to), [])
+
+  return [loc, navigate]
+}
--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import './styles.css'
 import ProjectsPage from 'pages/ProjectsPage'
 import TasksPage from 'pages/TasksPage'
 import StatisticsPage from 'pages/StatisticsPage/StatisticsPage'
-import { Switch, Route } from 'wouter'
+import { Router, Switch, Route } from 'wouter'
+import { useHashLocation } from 'hooks'
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={ProjectsPage} />
-      <Route path="/statistics/:projectId?" component={StatisticsPage} />
-      <Route path="/:projectId" component={TasksPage} />
-    </Switch>
+    <Router hook={useHashLocation}>
+      <Switch>
+        <Route path="/" component={ProjectsPage} />
+        <Route path="/statistics/:projectId?" component={StatisticsPage} />
+        <Route path="/:projectId" component={TasksPage} />
+      </Switch>
+    </Router>
   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-//serviceWorker.register()
+serviceWorker.register()
